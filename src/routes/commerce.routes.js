ad6c02ab95df6b316d47d503ff6d1ce5cdc8a788
Gustavo Guallpa <49213432+gguallpa97@users.noreply.github.com>
2025-12-30 21:02:04 +0000

    Fix json

--- src/routes/commerce.routes.js
+++ src/routes/commerce.routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const router = express.Router();
-
 /**
  * Simula delay (latencia de red)
  * @param {number} ms
@@ -19,25 +17,94 @@ const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
  *   }
  * }
  */
+
+const AssociationStatus = Object.freeze({
+    ASOCIACION_CORRECTA: 'Asociación Correcta',
+    ASOCIACION_INCORRECTA: 'Asociación Incorrecta'
+});
 router.post('/enroll', async (req, res) => {
     try {
         const { tid, ruc, code } = req.body;
-
 
         // Simular delay (2 segundos)
         await sleep(2000);
 
 
+        //CASE TWO
+        if (tid === '88888888') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'POS no asociado'
+            });
+        }
+
+        //CASE THREE
+        if (code === '300') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'Comercio bloqueado'
+            });
+        }
+
+        //CASE FOUR
+        if (code === '400') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'Comercio Inactivo'
+            });
+        }
+
+
+
+        //CASE FIVE
+        if (ruc === '1899060366001') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'RUC no existente'
+            });
+        }
+
+
+        //CASE SIX
+        if (code === '600' && ruc === '1899060377001') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'Comercio no pertenece al RUC'
+            });
+        }
+
+        //CASE SEVEN
+        if (tid === '55526987' && code === '700') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'Terminal no pertenece al comercio'
+            });
+        }
+
+        //CASE EIGHT
+        if (code === '800') {
+            return res.status(400).json({
+                message: AssociationStatus.ASOCIACION_INCORRECTA,
+                description: 'El comercio no dispone de una Cuenta de BA'
+            });
+        }
 
         console.log('📦 Commerce recibido:', tid, ruc, code);
-
+        //CASE ONE
         return res.status(200).json({
-            message: 'Commerce enrolado correctamente',
+            message: AssociationStatus.ASOCIACION_CORRECTA,
+            description: 'Procesamiento exitoso',
             commerce: {
+                name: 'Comercio Ejemplo S.A.',
+                address: 'Av. Siempre Viva 123',
+                phone: '098185769',
+                city: 'Quito',
+                legalRepresentative: 'Juan Pérez',
+                activityType: 'Venta al por menor',
+                cuenta: '1234567890',
                 tid,
                 ruc,
                 code
-
             }
         });
 
